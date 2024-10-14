23a5956ac5f35025cc99895fa88eb5029e4d29e5
0097eo <emmanuelokello294@gmail.com>
2024-10-14 12:04:50 +0300

    fix context of the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import Navbar from "./components/Navbar"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from "./pages/Home";
+import AboutPage from "./pages/About";
 
 const App = () => {
   return (
     <>
-    <Navbar />
     <Router>
+    <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </Router>
     </>

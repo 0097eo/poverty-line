ec75e13429b585c9ca4153b61005ed22fc3c54e8
0097eo <emmanuelokello294@gmail.com>
2024-10-14 10:13:12 +0300

    setting up routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
-
+import Navbar from "./components/Navbar"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from "./pages/Home";
 
 const App = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold underline text-blue-600">
-        Hello, welcome to povertyline
-      </h1>
-      <p className="mt-4 text-gray-700">
-        This is where all the magic starts.
-      </p>
-    </div>
+    <>
+    <Navbar />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+    </>
+    
   )
 }
 
